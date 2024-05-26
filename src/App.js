213367ad7d396c8eb73a7a6a1ff0ad0c842a5f88
Shiwang-Sharma98 <sshiwang87@gmail.com>
2024-05-26 11:41:44 +0530

    first react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { filterData } from "./data";
+import { apiUrl } from "./data";
+import Nav from "./components/Nav";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import { toast } from "react-toastify";
+import Spinnner from "./components/Spinnner";
 
-function App() {
+const App = () => {
+
+  const [courses,setCourses] = useState(null);
+  const [loading,setLoading] = useState(true);
+  const [category,setcategory] = useState(filterData[0].title);  
+
+  async function getData(){
+    setLoading(true);
+    try{
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      setCourses(output.data);
+    }
+    catch(error){
+      toast.error("network problem");
+    }
+    setLoading(false);
+  }
+
+  useEffect(()=>{
+    getData();
+  },[])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col min-h-screen bg-indigo-900">
+      <div>
+        <Nav></Nav>
+      </div>
+      <div className="bg-indigo-900">
+          <div>
+            <Filter filterData={filterData} category={category} setcategory={setcategory}></Filter>
+          </div>
+          <div className="w-11/12 max-w-[1200px] flex flex-wrap mx-auto justify-center items-center min-h-[50vh]">
+            {
+              loading ? (<Spinnner/> ): (<Cards courses={courses} category={category}></Cards>)
+            }
+          </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = ()=>{
+    return(
+        <div>
+            <nav className="bg-indigo-600 py-4 ">
+                <h1 className="font-bold text-center text-white text-3xl">Top Courses</h1>
+            </nav>
+        </div>
+    );
+}
+
+export default Nav;
